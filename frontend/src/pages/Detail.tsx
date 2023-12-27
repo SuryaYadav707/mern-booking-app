dbc4809a5f4a6291640dd515163b08e4f356b6de
SuryaYadav707 <yadavsurya581@gmail.com>
2023-12-27 14:50:47 +0530

    add hotel detial and e2e test

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "./../api-client";
 import { AiFillStar } from "react-icons/ai";
-import GuestInfoForm from "../form/ManageHotelForm/GuestInfoForm/GuestInfoForm";
+import GuestInfoForm from "../form/GuestInfoForm/GuestInfoForm";
 
 const Detail = () => {
   const { hotelId } = useParams();
